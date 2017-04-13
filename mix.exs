f520536c92df3fb110a54a3063f5e14c546b294d
Max Strange <max.strange@synapse.com>
2017-04-13 09:45:37 -0700

    updated version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Expyplot.Mixfile do
 
   def project do
     [app: :expyplot,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
