4c16d3a5581e74e809960354e7d1b258085bb66c
Max Strange <max.strange@synapse.com>
2017-04-13 09:43:26 -0700

    hist now returns the list it was given, so you can pipe with it

--- lib/plot.ex
+++ lib/plot.ex
@@ -16,26 +16,12 @@ defmodule Expyplot.Plot do
   http://matplotlib.org/api/pyplot_api.html
 
   #TODO: This needs to return something - so it needs to communicate with a python process that is running as a server
-
-  ## Examples
-
-      iex> Expyplot.Plot.hist([50, 100], normed: 1, facecolor: :green, alpha: 0.75)
-      :ok
-
-      iex> Expyplot.Plot.hist([20])
-      :ok
-
-      iex> Expyplot.Plot.hist([5, 10, 15], log: true)
-      :ok
-
-      iex> Expyplot.Plot.hist([23, 45], data: %{money: 5, people: [24, "money"]})
-      :ok
   """
   def hist(x, opts \\ [bins: nil, range: nil, normed: false, weights: nil, cumulative: false, bottom: nil, histtype: :bar, align: :mid, orientation: :vertical,
                        rwidth: nil, log: false, color: nil, label: nil, stacked: false, hold: nil, data: nil], kwargs \\ []) do
     code = Codebuilder.build_code(funcname: "plt.hist", nonnamed: [x], named: opts ++ kwargs)
     Server.Commapi.add_code code
-    :ok
+    x
   end
 
   @doc """
